db60a784185441e2fdec691cda278d87b845c3e3
Tiago Debastiani <debastianister@gmail.com>
2016-06-01 15:35:58 -0300

    Implementado função 'criarVetor' e função 'combSort' para vetor;

--- main.c
+++ main.c
@@ -2,16 +2,59 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX 40 
+#define MAX 10 
 
 typedef struct _contato{
 	char nome[40];
 	char fone[30];
 }TpContato;
 
+void combSort(int a[], int tam){
+	int gap = tam;
+	int swap = 1;
+	int i, j, aux;
+	printf("gap:swap= %d:%d\n", gap,swap);
+	
+	while(gap > 1 || swap == 0){
+		gap = gap / 1.3;
+		if (gap < 1) gap = 1;
+
+		swap = 0;
+		i = 0;
+		j = i+gap;
+		for(i=0, j=gap; j < tam; i++, j++){
+			printf("i:j= %d:%d\n", i,j);
+			if (a[i] > a[j]){
+				printf("troca= %d<->%d\n",a[i],a[j]);
+				aux  = a[i];
+				a[i] = a[j];
+				a[j] = aux;
+				swap = 1;
+			}	
+		}
+	}
+}
+
+void criarVetor(int v[]){
+	int i;
+	for (i=0; i<MAX; i++){
+		v[i] = (rand()%MAX)*i;
+	}
+}
+
+void imprimeVetor(int v[]){
+	int i;
+	for (i=0;i<MAX;i++){
+		printf("%d\t", v[i]);
+	}
+	printf("\n");
+}
+
 int main(){
 	
 	int fn;
+	int vTeste[MAX];
+
 	system("clear");
 	do{
 		printf(
@@ -33,10 +76,15 @@ int main(){
 			case 2:
 				system("clear");
 				printf("\t2) Cirar Vetor\n");
+				criarVetor(vTeste);
 				break;
 			case 3:
 				system("clear");
 				printf("\t3) Ordenacao com Comb Sort\n");
+				imprimeVetor(vTeste);
+				combSort(vTeste,MAX);
+				printf("\n");
+				imprimeVetor(vTeste);
 				break;
 			case 4:
 				system("clear");
